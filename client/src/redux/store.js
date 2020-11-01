151cc38f5d4cf1f8238dc021e2475000d84d55c7
thantmyat <thantmyat1234@gmail.com>
2020-11-01 19:46:31 +0630

    update logger

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,7 +3,11 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './rootReducer';
 
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+	middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
