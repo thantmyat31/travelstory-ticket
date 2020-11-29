8923f3171e7fb90e0841a4750924f404aa997820
thantmyat <thantmyat1234@gmail.com>
2020-11-29 16:52:32 +0630

    update server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require("path");
 
 require('dotenv').config();
 
@@ -26,7 +27,7 @@ app.use('/api/user', require('./server/routes/userRouter'));
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client', 'build')));
-
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     });
