aee686a1b143940d7833a491b06537bccc3b1011
thantmyat <thantmyat1234@gmail.com>
2020-11-29 16:10:59 +0630

    changed proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:2020',
+      target: 'http://localhost:8000',
       changeOrigin: true,
     })
   );
