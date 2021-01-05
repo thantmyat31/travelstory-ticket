8ee9892413391ba52dd41d7140b6fe43b13cffa8
thantmyat <thantmyat1234@gmail.com>
2021-01-05 10:27:26 +0630

    updated subscriber dashboard/profile

--- client/src/pages/Auth/SubscriberDBPage/SBDetails/SBDetails.jsx
+++ client/src/pages/Auth/SubscriberDBPage/SBDetails/SBDetails.jsx
@@ -1,10 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import Title from '../../../../components/Title/Title';
+import CardRow from './../../../../components/CardRow/CardRow';
+import Loading from '../../../../components/Loading/Loading';
+import styles from './SBDetails.module.css';
 
 const SBDetails = () => {
+    const { user } = useSelector(state => state.user);
+
+    const renderProfile = (user) => {
+        if(!user) return <Loading />;
+        return (
+            <>
+                <table className={styles.table}>
+                    <tbody>
+                        <tr>
+                            <td>Name</td>
+                            <td>{user.displayName}</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>{user.email}</td>
+                        </tr>
+                        <tr>
+                            <td>Role</td>
+                            <td>{user.role}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </>
+        )
+    }
+
     return ( 
-        <div>
-            Subscriber details
-        </div>
+        <>
+            <Title title="Profile" />
+            {renderProfile(user)}
+        </>
      );
 }
  
